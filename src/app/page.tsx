b6c701aea220684e1e35b3963d3015f385137a3e
RSsunny <sunnysharif154@gmail.com>
2025-02-06 22:16:34 +0600

    FOOTER ADD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Header from '@/shared/Header'
-
+import Footer from '@/shared/Footer'
 export default function Home() {
   return (
    <main>
     <Header/>
+    <Footer/>
    </main>
   );
 }
---
+++ src/shared/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="border-t px-32 py-10 text-center mt-40">
+      <h2 className="text-3xl font-bold pb-5">Next.js Tutorial</h2>
+      <p className="w-[800px] mx-auto">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda ea
+        fugit aperiam adipisci! Provident necessitatibus vitae rem odio
+        similique.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
